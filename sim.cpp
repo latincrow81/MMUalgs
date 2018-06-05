a9052db26d7cd452ecc33e21459936a3d13cc481
Mauricio <latincrow81@gmail.com>
2018-06-05 00:48:16 -0500

    all set for algorith simulation

--- sim.cpp
+++ sim.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <sstream>
 #include <vector>
+#include <array>
 #include <cmath>
 #include <string>
 
@@ -14,9 +15,11 @@ int main(int argc, char* argv[])
 
     unsigned int pageBase;
     unsigned int * pageTable;
-    unsigned int input;
+    unsigned int * frameArray;
+    string page;
     
     vector <unsigned int> request;
+    string requestFile;
 
     // Check the number of parameters
     if (argc < 5) {
@@ -49,7 +52,7 @@ int main(int argc, char* argv[])
         return 1;
     }
 
-    string algorithmName [] {"FIFO", "LRU", "OPTIMO"};
+    string algorithmName [] {"FIFO", "LRU", "OPTIMAL"};
 
     escalarA = log2 (pageSize);
     escalar = addrBits - escalarA;
@@ -69,30 +72,66 @@ int main(int argc, char* argv[])
 
         for(int i = 0; i <= pageNumber; i++)
         {
-            /* save base address as pageName */
+            // save base address as pageName
             pageBase = i*pageSize;
             pageTable[i] = pageBase;
-        }
-
-        // print pages base address
-        for(int i = 0; i <= pageNumber; i++)
-        {
-            /* print page # and base */
+            // print pages base address
             cout << "Page # " << dec << i << " Base @ " << hex << pageTable[i] << endl;
-            
         }
 
-        //delete[] pageTable;
-        //cout << "Page table cleared from memory" << endl;
-
     }
 
-    ifstream namefile(argv[5]);
-    while( namefile >> input){
-        request.push_back(input);
-        cout << "Read " << request.size() << " addresses successfully\n";
+    istringstream ssReq(argv[5]);
+
+    if (!(ssReq >> requestFile)) {
+        cerr << "Invalid input " << argv[5] << '\n';
+        cerr << "Usage: " << argv[0] << " [bits of address] [page size (bytes)] [frames] [algorithm] [access sequence]" << std::endl;
+        return 1;
     }
+    
+    ifstream namefile(requestFile);
+    
+    if (namefile.is_open())
+    {
+        while ( getline (namefile, page) )
+        {
+            unsigned int pageNumber = stoul(page,nullptr,0);
+            request.push_back(pageNumber);
+            cout << "page " << page << " registered" << '\n';
+        }
+        namefile.close();
+    }else{ 
+        cerr << "Unable to open file"; 
+        return 1;
+    }
+    
+    istringstream ssFrames(argv[3]);
+
+    if (!(ssFrames >> frames)) {
+        cerr << "Invalid number " << argv[1] << '\n';
+        cerr << "Usage: " << argv[0] << " [bits of address] [page size (bytes)] [frames] [algorithm] [access sequence]" << std::endl;
+        return 1;
+    }
+
+    frameArray = new (nothrow) unsigned int[frames];
 
     
+    // simulate using FIFO
+    if (algorithm == 0){
+        
+    }
+
+    // simulate using LRU
+    if (algorithm == 1){
+
+    }
+
+    // simulate using OPTIMAL
+    if (algorithm == 2){
+
+    }
+
+    delete[] pageTable;
+    delete[] frameArray;
     return 0;
 }
